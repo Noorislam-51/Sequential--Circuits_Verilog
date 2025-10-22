dd1f14e7e224eb150d5b1dbf434d1747bffc5c16
Noor Islam <noorislam5114@gmail.com>
2025-10-22 11:30:00 +0530

    kajdfkl

--- REGISTER/register.v
+++ REGISTER/register.v
@@ -17,8 +17,8 @@ endmodule
 
 module register_pipo_bh(q,clk,reset,d);
   input clk ,reset;
-  input [7:0]d;
-  output reg[7:0]q;
+  input [3:0]d;
+  output reg[3:0]q;
   
   always@(posedge clk)begin
     if(reset)
@@ -31,17 +31,17 @@ endmodule
 
 module register_pipo_st(q,clk,reset,d);
   input clk ,reset;
-  input [7:0]d;
-  output reg[7:0]q;
+  input [3:0]d;
+  output reg[3:0]q;
   
   dff dff0(.q(q[0]), .clk(clk), .reset(reset), .d(d[0]));
   dff dff1(.q(q[1]), .clk(clk), .reset(reset), .d(d[1]));
   dff dff2(.q(q[2]), .clk(clk), .reset(reset), .d(d[2]));
   dff dff3(.q(q[3]), .clk(clk), .reset(reset), .d(d[3]));
-  dff dff4(.q(q[4]), .clk(clk), .reset(reset), .d(d[4]));
-  dff dff5(.q(q[5]), .clk(clk), .reset(reset), .d(d[5]));
-  dff dff6(.q(q[6]), .clk(clk), .reset(reset), .d(d[6]));
-  dff dff7(.q(q[7]), .clk(clk), .reset(reset), .d(d[7]));
+  // dff dff4(.q(q[4]), .clk(clk), .reset(reset), .d(d[4]));
+  // dff dff5(.q(q[5]), .clk(clk), .reset(reset), .d(d[5]));
+  // dff dff6(.q(q[6]), .clk(clk), .reset(reset), .d(d[6]));
+  // dff dff7(.q(q[7]), .clk(clk), .reset(reset), .d(d[7]));
  
   
   end
@@ -109,3 +109,59 @@ always@(posedge clk)begin
     q = {serial_in, q[3:1]};
   end
 endmodule
+
+
+
+// -----------------------------------------------------------
+// PISO
+// -----------------------------------------------------------
+ 
+  module combination(y,a,b,c,d);
+    input a,b,c,d;
+    output y;
+    wire w1,w2;
+    and and1(w1,a,b);
+    and and2(w2,c,d);
+    or or1(y,w1,w2);
+  endmodule
+
+
+
+module shift_register_st(serial_out,parallel_in,reset,load,clk);
+    input clk ,load;
+    input [3:0]parallel_in;
+    output serial_out;
+    wire [3:0] q, d;
+
+  combination c1(.y(d[1]), .a(q[0]), .b(~load), .c(load), .d(parallel_in[1]));
+  combination c2(.y(d[2]), .a(q[1]), .b(~load), .c(load), .d(parallel_in[2]));
+  combination c3(.y(d[3]), .a(q[2]), .b(~load), .c(load), .d(parallel_in[3]));
+
+  dff dff0(.q(q[0]), .clk(clk), .reset(reset), .d(parallel_in[0]));
+  dff dff1(.q(q[1]), .clk(clk), .reset(reset), .d(d[1]));
+  dff dff2(.q(q[2]), .clk(clk), .reset(reset), .d(d[2]));
+  dff dff3(.q(q[3]), .clk(clk), .reset(reset), .d(d[3]));
+	// Serial output from MSB
+  assign serial_out = q[3];
+
+  endmodule
+
+  module shift_register_bh(serial_out,parallel_in,reset,load,clk);
+    input clk ,load,reset;
+    input [3:0]parallel_in;
+    output reg serial_out;
+    reg [3:0]  q;
+
+  always@(posedge clk or posedge reset)
+    if (reset)begin
+      q<=4'b0000;
+      serial_out <= 1'b0;
+      end
+    else if (load)begin
+      q<=parallel_in;
+    end 
+    else begin 
+      serial_out<= q[3];
+      q<= {serial_out,q[3:1]};
+  end
+  endmodule 
